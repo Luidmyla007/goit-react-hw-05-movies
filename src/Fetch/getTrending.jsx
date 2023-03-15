412b67bfc368ae6b03cf66f69811f32a689bf5ce
Liudmyla007 <ldatsenko3@gmail.com>
2023-03-15 20:08:22 +0200

    files system

---
+++ src/Fetch/getTrending.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+const TOKEN = '51114562faac57108ae3113fba230ec4';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+async function getTrending(page) {
+    const  result =  await axios('trending/movie/day', {
+    params: {
+      api_key: TOKEN,
+      page,
+    },
+    });
+    console.log(result);
+     const trendsMov = result.data.results.map(
+    ({ id, title, poster_path, vote_average }) => {
+      return { id, title, poster_path, vote_average };
+    }
+  );
+    return trendsMov;
+};
+
+export default getTrending;
