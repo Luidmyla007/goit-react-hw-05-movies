c4917e43fb1593df08ab4608143db92877768e26
Liudmyla007 <ldatsenko3@gmail.com>
2023-03-16 11:23:59 +0200

    add get responses

--- src/Fetch/getSearchMovies.jsx
+++ src/Fetch/getSearchMovies.jsx
@@ -3,10 +3,12 @@ async function getSearchMovies(query, page = 1) {
   const url = `https://api.themoviedb.org/3/search/movie`;
   const filter = `?api_key=${key}&query=${query}&language=en-US&page=${page}&include_adult=false`;
 
-  const response = await fetch(`${url}${filter}`);
-  const data = await response.json();
-
-  return data;
+  const response = await fetch(`${url}${filter}`).then(response => {
+      if (!response.ok) {
+        throw new Error('Oops, there is no movie with that name');
+      }
+      return response.json();
+    }) ;  
 }
 
 export default getSearchMovies;
