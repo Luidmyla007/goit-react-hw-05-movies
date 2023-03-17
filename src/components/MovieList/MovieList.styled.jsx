40081ad2df1e75f69dc3a881e7ddb1960dec1443
Liudmyla007 <ldatsenko3@gmail.com>
2023-03-17 21:46:51 +0200

    MovieList styles

--- src/components/MovieList/MovieList.styled.jsx
+++ src/components/MovieList/MovieList.styled.jsx
@@ -14,14 +14,24 @@ export const MovTitle = styled.p`
   font-weight: 700;
   text-align: center;
   color: rgba(9, 60, 61, 1);
+   text-shadow: 2px 2px 4px rgba(143, 199, 201, 1);
 
 `
 export const MovItem = styled.li` 
  width: calc((100%-80px) / 5);
   transform: scale(1);
   border-radius: 12px;
+
+  &:hover {
+    transform: scale(1.01);
+  }
+  &:hover > p {
+    opacity: 1;
+  }
  
   img {  
+    border-radius: 5px;
+    box-shadow: 10px 10px 8px 2px rgba(0, 0, 0, 0.3);
     height: 100%;
     width: 100%;
 `  
